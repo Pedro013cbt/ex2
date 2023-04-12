b3e787a6838237ee5efc7153a9ee4c9808afa954
Pedro013cbt <130083855+Pedro013cbt@users.noreply.github.com>
2023-04-12 20:38:45 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1
+{
+    internal class Program
+     // exercicio 2
+        static void Main(string[] args)
+        {
+        int l;
+        int a;
+        Console.WriteLine("digite o valor da aresta: ");
+        l = int.Parse(Console.ReadLine());
+        a = l * l;
+        Console.WriteLine("o valor da aresta é :{0}", a);
+            
+        
+        }
+    }
